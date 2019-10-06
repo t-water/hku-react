3ae5fefb4f8b3c67c098d6852cb4640bfab0fe11
Tyler Waterman <tylerdwaterman@gmail.com>
2019-10-06 12:49:22 -0400

    Fetch from Server

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -4,12 +4,13 @@ import {Link} from 'react-router-dom';
 import CommentForm from './CommentFormComponent';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDish({dish}){
 	return(
 		<div className="col-12 col-md-5 m-1">
 			<Card>
-				<CardImg top src = {dish.image} alt = {dish.name}/>
+				<CardImg top src = {baseUrl + dish.image} alt = {dish.name}/>
 				<CardBody>
 					<CardTitle>{dish.name}</CardTitle>
 					<CardText>{dish.description}</CardText>
