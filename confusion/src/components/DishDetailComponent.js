df52df0e4da87bac22e04d3d89f40eb22f89ae57
Tyler Waterman <tylerdwaterman@gmail.com>
2019-10-06 17:51:56 -0400

    Fetch Post Comment

--- confusion/src/components/DishDetailComponent.js
+++ confusion/src/components/DishDetailComponent.js
@@ -20,7 +20,7 @@ function RenderDish({dish}){
 	)
 }	
 
-function RenderComments({comments, addComment, dishId}){
+function RenderComments({comments, postComment, dishId}){
 	if(comments != null){
 		return(
 			<div className="col-12 col-md-5 m-1">
@@ -35,7 +35,7 @@ function RenderComments({comments, addComment, dishId}){
 						)
 					})}
 				</ul>
-				<CommentForm dishId = {dishId} addComment = {addComment}/>
+				<CommentForm dishId = {dishId} postComment = {postComment}/>
 			</div>
 		)
 	}else{
@@ -78,7 +78,7 @@ const DishDetail = (props) => {
 	        <div className="row">
 	            <RenderDish dish={props.dish} />
 	            <RenderComments comments={props.comments} 
-	            				addComment = {props.addComment}
+	            				postComment = {props.postComment}
 	            				dishId = {props.dish.id}
 	            />
 	        </div>
