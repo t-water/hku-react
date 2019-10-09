c98b4e758ad615fb8162a12d05b075750af9210e
Tyler Waterman <tylerdwaterman@gmail.com>
2019-10-09 16:24:49 -0400

    Assignment 4

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { postComment, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
+import { postComment, postFeedback, fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
@@ -23,6 +23,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => ({
   postComment: (dishId, rating, author, comment) => dispatch(postComment(dishId, rating, author, comment)),
+  postFeedback: (firstname, lastname, telnum, email, agree, contactType, message) => dispatch(postFeedback(firstname, lastname, telnum, email, agree, contactType, message)),
   fetchDishes: () => {dispatch(fetchDishes())},
   resetFeedbackForm: () => {dispatch(actions.reset('feedback'))},
   fetchComments: () => dispatch(fetchComments()),
@@ -78,7 +79,8 @@ class Main extends Component {
                 <Route path= "/home" component = {HomePage}/>
                 <Route exact path= "/menu" component = {() => <Menu dishes = {this.props.dishes}/>}/>
                 <Route path = "/menu/:dishId" component = {DishWithId}/>
-                <Route exact path = "/contactus" component = {() => <Contact resetFeedbackForm = {this.props.resetFeedbackForm}/>}/>
+                <Route exact path = "/contactus" component = {() => <Contact resetFeedbackForm = {this.props.resetFeedbackForm}
+                                                                             postFeedback = {this.props.postFeedback}/>}/>
                 <Route path = "/aboutus" component = {() => <About leaders = {this.props.leaders.leaders}
                                                                    isLoading = {this.props.leaders.isLoading}
                                                                    errMess = {this.props.leaders.errMess}/>}/>
